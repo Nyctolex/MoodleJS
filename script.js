e3b9bf61c29dcc2ec5ec8b7c08728feb27b94f79
Alex Chen <nycolex@gmail.com>
2022-03-26 18:16:11 +0300

    Expand feature fixed

--- script.js
+++ script.js
@@ -10,17 +10,50 @@ function saveData() {
         lpassword: password,
         autologin: autologin_value
     }
-    chrome.storage.sync.set(data, function() { console.log("Value is set to " + data); });
+    chrome.storage.sync.set(data, function() { });
 }
 
+
+function showSettings() {
+    settingTab = document.getElementById("settingsTab");
+        settingTab.style.maxHeight = "400px";
+        arrow= document.getElementById("moreSettings");
+        arrow.style.transition = '.4s';
+        document.getElementById('moreSettingText').style.visibility = "hidden";
+        arrow.style.webkitTransform = 'rotate(180deg)';
+ 
+}
+function toggleShowSettings() {
+    settingTab = document.getElementById("settingsTab");
+    if(settingTab.style.maxHeight == '' || settingTab.style.maxHeight =='0px') {
+
+        settingTab.style.maxHeight = "400px";
+        arrow= document.getElementById("moreSettings");
+        arrow.style.transition = '.4s';
+        document.getElementById('moreSettingText').style.visibility = "hidden";
+        arrow.style.webkitTransform = 'rotate(180deg)';
+    }
+    else {
+        settingTab.style.maxHeight = "0";
+        arrow= document.getElementById("moreSettings");
+        arrow.style.transition = '.5s';
+        arrow.style.webkitTransform = 'rotate(0deg)';
+    }
+}
 
 window.onload = function() {
     document.getElementById('moodleBtn').addEventListener("click", ()=> {window.open("https://moodle.tau.ac.il/login/index.php"); });
     document.getElementById('studentInfoBtn').addEventListener("click", ()=> {window.open("https://www.ims.tau.ac.il/Tal"); });
-    document.getElementById('moreSettings').addEventListener("click", showSettings);
-    chrome.storage.sync.get(function (result) { for (var k in result) { if (result.hasOwnProperty(k)) { if (k != "autologin") { document.getElementById(k).value = result[k]; } else { document.getElementById(k).checked = result[k]; } } } });
+    document.getElementById('moreSettings').addEventListener("click", toggleShowSettings);
 
-    chrome.storage.sync.get(function(result) { for(var k in result) { if(result.hasOwnProperty(k)) { if(k != "autologin") { if(document.getElementById(k).value == "") { showSettings(); document.getElementById("autologin").checked = false; } } } } });
+    chrome.storage.sync.get(function (result) { for (var k in result) { if (result.hasOwnProperty(k)) { if (k != "autologin") { document.getElementById(k).value = result[k]; } else { document.getElementById(k).checked = result[k]; } } } });
+    chrome.storage.sync.get(function(result) { for(var k in result) { 
+        if(result[k] == "" && k!= "autologin") {  
+        console.log("expand");    
+        document.getElementById("autologin").checked = false;
+        saveData();
+        showSettings();
+    }   } });
     document.getElementById("autologin").addEventListener('change', saveData);
     document.getElementById("lusername").addEventListener('change', saveData);
     document.getElementById("lid").addEventListener('change', saveData);
@@ -28,29 +61,3 @@ window.onload = function() {
 
 };
 
-
-function showSettings() {
-    settingTab = document.getElementById("settingsTab");
-    if(settingTab.style.maxHeight == '' || settingTab.style.maxHeight =='0px') {
-
-        // settingTab.style.display = "block";
-        // settingTab.style.visibility = "visible";
-        settingTab.style.maxHeight = "400px";
-        // settingTab.style.opacity = 1;
-        arrow= document.getElementById("moreSettings");
-        // settingTab.style.transition = '.1s';
-        arrow.style.transition = '.4s';
-        document.getElementById('moreSettingText').style.visibility = "hidden";
-        arrow.style.webkitTransform = 'rotate(180deg)';
-    }
-    else {
-        // settingTab.style.display = "none";
-        // settingTab.style.visibility = "hidden";
-        settingTab.style.maxHeight = "0";
-        // settingTab.style.opacity = 0;
-        arrow= document.getElementById("moreSettings");
-        arrow.style.transition = '.5s';
-        // document.getElementById('moreSettingText').style.visibility = "visible";
-        arrow.style.webkitTransform = 'rotate(0deg)';
-    }
-}
